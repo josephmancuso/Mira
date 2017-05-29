bd4272c2c58879963a82b43f2c93ca9d31b5dca7
Joseph Mancuso <joseph@orderinghero.com>
2017-05-29 13:28:22 -0400

    fixed lack of option in config file throwing error

--- application/Routes/routes.php
+++ application/Routes/routes.php
@@ -3,7 +3,11 @@ session_start();
 error_reporting(E_ALL & ~E_NOTICE);
 require_once 'init.php';
 require_once '../models/models.php';
-foreach ($config['templates'] as $template) {
-    include_once("../app/$template/routes/routes.php");
+
+if ($config['templates']) {
+    foreach ($config['templates'] as $template) {
+        include_once("../app/$template/routes/routes.php");
+    }
 }
 
+// routes go here

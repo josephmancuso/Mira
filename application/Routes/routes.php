8a71bf64dba918c4518820b79088ba7efe376769
Joseph Mancuso <joseph@orderinghero.com>
2017-06-20 22:02:03 -0400

    added middleware to main routes file

--- application/Routes/routes.php
+++ application/Routes/routes.php
@@ -1,14 +1,21 @@
 <?php
 session_start();
-error_reporting(E_ALL & ~E_NOTICE);
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
 require_once 'init.php';
 require_once '../models/models.php';
+
+require_once '../forms/forms.php';
+
+if ($config['middleware']) {
+    foreach ($config['middleware'] as $template) {
+        require_once("../app/$template/middleware/middleware.php");
+    }
+}
 
 if ($config['templates']) {
     foreach ($config['templates'] as $template) {
         include_once("../app/$template/routes/routes.php");
     }
 }
-
 
 // routes go here

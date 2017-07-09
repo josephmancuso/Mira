7f5b0bf3c407730c22e9864b76e2273139869b35
Joseph Mancuso <joseph@orderinghero.com>
2017-07-09 12:25:30 -0400

    added providers config file

--- config/providers.php
+++ config/providers.php
@@ -2,15 +2,12 @@
 
 return [
     'Providers' => [
-        'Application\Providers\Depends',
-        'Application\Providers\Project',
-        'Application\Providers\Models',
-        'Application\Providers\RaiseHttp',
-        'Application\Providers\TestModel',
-        'Application\Providers\AppModels',
-        'Application\Providers\Forms',
-        'Application\Providers\Render',
-        'Application\Providers\BlogRender',
-        'Application\Providers\Controller',
+        'Vendor\MiraFramework\Project', // core
+        'Vendor\MiraFramework\Models', // core
+        'Vendor\MiraFramework\AppModels', // core
+        'Vendor\MiraFramework\Forms', // core
+        'Vendor\MiraFramework\Render', // core
+        'Vendor\MiraFramework\Shorthand', // core
+        'Vendor\MiraFramework\Controller', // core
     ]
 ];

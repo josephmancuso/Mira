71986f7be6bec93565fc1b22a86d5f9c754d38f2
Joseph Mancuso <joseph@orderinghero.com>
2017-07-22 22:12:12 -0400

    removed providers that psr-4 made obsolete

--- config/providers.php
+++ config/providers.php
@@ -2,12 +2,8 @@
 
 return [
     'Providers' => [
-        'Vendor\MiraFramework\Project', // core
-        'Vendor\MiraFramework\Models', // core
-        'Vendor\MiraFramework\AppModels', // core
-        'Vendor\MiraFramework\Forms', // core
-        'Vendor\MiraFramework\Render', // core
-        'Vendor\MiraFramework\Shorthand', // core
-        'Vendor\MiraFramework\Controller', // core
+        'Application\Providers\Forms', // core
+        'Application\Providers\Shorthand', // core
+        'Application\Providers\Comet', // core
     ]
 ];

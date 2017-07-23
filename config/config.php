537c3e95d7f7b60a66879f3253c724c9b739980f
Joseph Mancuso <joseph@orderinghero.com>
2017-07-22 22:11:38 -0400

    updated config file to remove unneeded options

--- config/config.php
+++ config/config.php
@@ -13,49 +13,21 @@ return [
 
     'database' => [
         'host' => 'localhost',
-        'username' => 'database_name',
+        'username' => 'database_username',
         'password' => 'database_password',
     ],
 
     /*
     |--------------------------------------------------------------------------
-    | Templates
+    | Apps
     |--------------------------------------------------------------------------
     |
-    | Add any apps to this configuration in order for mira to search through
-    | and find the correct route files. If the app is not inside this setting
-    | then it will simply not match the correct URL's. 
+    | Any apps you want Mira to register, put here. Putting apps here will
+    | auto register both the controllers and routes.
     |
     */
 
-    'templates' => [
-        'mira',
+    'Apps' => [
+        'Mira'
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Models
-    |--------------------------------------------------------------------------
-    |
-    | Any apps you have will load all the models to be available in any of
-    | the route files. Put the app name in the setting below so Mira
-    | can recognize the models.
-    |
-    */
-
-    'models' => [],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Middleware
-    |--------------------------------------------------------------------------
-    |
-    | Add the folder inside your middleware folder inside your application.
-    | Middlware can be found at application > middleware > middleware_name.
-    | Inside this setting, just put the middleware_name and it will 
-    | connect to the autoload.php file. inside the app.
-    |
-    */
-
-    'middleware' => [],
 ];
